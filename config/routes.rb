2253ac13d1d00f2fcc098a21e91718176b9357ac
James Ferguson <ncjamesferguson@gmail.com>
2018-04-17 23:37:50 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,6 @@ Rails.application.routes.draw do
 
   post 'sessions/create', to: 'sessions#create'
 
-  get 'sessions/new', to: 'sessions#new' as: 'login'
+  get 'sessions/new', to: 'sessions#new' as: :login
 
 end

54188b38614dc8c457b73b96f7481a1b5f773e2a
James Ferguson <ncjamesferguson@gmail.com>
2018-04-17 20:41:31 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     if session[:username].nil? || session[:username].empty?
-      redirect_to login_path
+      redirect_to '/'
     else
     session[:username] = params[:username]
     redirect to secret_path
